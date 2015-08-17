d5895621892f457ddedaf4326fa8a3434f2f3d46
Frederick Pearson <fredp613@gmail.com>
2015-08-17 16:57:06 -0400

    fixes

--- fund_center_budget_subgrid_toggle.js
+++ fund_center_budget_subgrid_toggle.js
@@ -1,8 +1,12 @@
 Xrm.Page.getAttribute("gcbase_estimatedannualbudget").addOnChange(refreshBudgetGrid)
+Xrm.Page.getAttribute("gcbase_estimatedannualbudgetgrant").addOnChange(refreshBudgetGrid)
 
 function refreshBudgetGrid() {
 	console.log("budget grid should be refreshed");
 	Xrm.Page.data.entity.save()
 	var subgrid = Xrm.Page.ui.controls.get("AnnualBudgets");
-	subgrid .refresh();
+	if (subgrid) {
+		subgrid .refresh();
+	}
+	
 }
