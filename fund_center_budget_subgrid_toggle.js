3ecddd5924f7131669e83368eae9a1bef3eac483
Frederick Pearson <fredp613@gmail.com>
2015-09-26 16:44:57 -0400

    test

--- fund_center_budget_subgrid_toggle.js
+++ fund_center_budget_subgrid_toggle.js
@@ -1,7 +1,7 @@
 Xrm.Page.getAttribute("gcbase_estimatedannualbudget").addOnChange(refreshBudgetGrid)
 Xrm.Page.getAttribute("gcbase_startdate").addOnChange(refreshBudgetGrid)
 Xrm.Page.getAttribute("gcbase_enddate").addOnChange(refreshBudgetGrid)
-Xrm.Page.getAttribute("statuscode").addOnChange(refreshIframe)
+
 
 
 function refreshBudgetGrid() {
@@ -13,10 +13,3 @@ function refreshBudgetGrid() {
 	}
 }
 
-function refreshIframe() {
-	console.log("iframe refresh")
-	var src = Xrm.Page.getControl("IFRAME_IFRAME_Confirmation").getInitialUrl()
-	console.log(src)
-	Xrm.Page.getControl("IFRAME_IFRAME_Confirmation").setSrc(src)
-
-}
